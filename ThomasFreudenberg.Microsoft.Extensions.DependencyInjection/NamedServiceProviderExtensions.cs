cf72bc46ca9704689d5ab0e8c2ecd749eda82fde
Thomas Freudenberg <info@thomasfreudenberg.com>
2020-05-25 14:44:09 +0200

    register multiple implementations per name

--- ThomasFreudenberg.Microsoft.Extensions.DependencyInjection/NamedServiceProviderExtensions.cs
+++ ThomasFreudenberg.Microsoft.Extensions.DependencyInjection/NamedServiceProviderExtensions.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ThomasFreudenberg.Microsoft.Extensions.DependencyInjection
 {
     public static class NamedServiceProviderExtensions
     {
-        public static TService? GetNamedService<TService>(this IServiceProvider serviceProvider, string reportName) where TService: class
+        public static TService? GetNamedService<TService>(this IServiceProvider serviceProvider, string serviceName) where TService: class
         {
             var registry = serviceProvider.GetService<NamedServiceRegistry<TService>>();
             if (registry == null)
@@ -14,20 +15,29 @@ namespace ThomasFreudenberg.Microsoft.Extensions.DependencyInjection
                 // be a NamedServiceRegistry
                 return null;
             }
-            var implementationType = registry.GetImplementationType(reportName);
+
+            var implementationType = registry.GetImplementationTypes(serviceName).FirstOrDefault();
+            if (implementationType == null)
+            {
+                return null;
+            }
 
             return (TService?)serviceProvider.GetService(implementationType);
         }
 
-        public static TService GetRequiredNamedService<TService>(this IServiceProvider serviceProvider, string reportName)
+        public static TService GetRequiredNamedService<TService>(this IServiceProvider serviceProvider, string serviceName)
         {
             var registry = serviceProvider.GetService<NamedServiceRegistry<TService>>();
             if (registry == null)
             {
-                throw new ArgumentException("No named service has been registered.");
+                throw new ArgumentException($"No service with name {serviceName} has been registered.", nameof(serviceName));
             }
 
-            var implementationType = registry.GetImplementationType(reportName);
+            var implementationType = registry.GetImplementationTypes(serviceName).FirstOrDefault();
+            if (implementationType == null)
+            {
+                throw new ArgumentException($"No service with name {serviceName} has been registered.", nameof(serviceName));
+            }
 
             return (TService)serviceProvider.GetRequiredService(implementationType);
         }
